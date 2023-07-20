bad1937a6cc50c64796f5c5c515671c9c628cc3c
BenPerraud <benjaminperraud@outlook.fr>
2023-07-20 19:32:27 +0200

    v_fs

--- src/backend/middleware/sharp-config.js
+++ src/backend/middleware/sharp-config.js
@@ -1,9 +1,20 @@
 const sharp = require("sharp")
+const fs = require("fs")
 
 module.exports = (req, res, next) => {
     const newPath = "processed_images\\"+req.file.filename
+    console.log(req.file)
+    const {path, filename} = req.file
+
     sharp(req.file.path)
         .resize(320, 240)
         .toFile(newPath)
+    
+    console.log(12)
+    fs.unlink(path, (err => {
+        if (err) console.log(err)})
+    )
+    
     next()
 }
+
